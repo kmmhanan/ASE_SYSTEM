59df00d99be3583500e1f255effc683e96d37b96
Kmm Hanan <kmm.hanan2@gmail.com>
2024-01-10 22:20:13 +0530

    repository changed

---
+++ app/citytaxi/lib/screens/passengerScreens/p_HomePage/p_confirmationPage.dart
@@ -0,0 +1,56 @@
+import 'package:citytaxi/constants/palette.dart';
+import 'package:citytaxi/screens/passengerScreens/p_HomePage/p_paymentPage.dart';
+import 'package:citytaxi/screens/passengerScreens/p_login_screen.dart';
+import 'package:citytaxi/screens/passwordChange/resetPasswordPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PConfirmationPage extends StatefulWidget {
+  const PConfirmationPage({super.key});
+
+  @override
+  State<PConfirmationPage> createState() => _PConfirmationPageState();
+}
+
+class _PConfirmationPageState extends State<PConfirmationPage> {
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(const Duration(milliseconds: 3500), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const PPaymentPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.mainColor60,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Palette.mainColor60,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo/icons/tick.png', height: 100, width: 100),
+            SizedBox(height: 25),
+            Text(
+              'Thank You For Your Confirmation!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Palette.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
